4d10bc18a5f6e200786214e2652153e3a46d90fb
Clément Chaine <cc.chaine@free.fr>
2022-06-05 11:17:39 +0200

    add riscv constants

--- include/riscv_pkg.sv
+++ include/riscv_pkg.sv
@@ -22,7 +22,64 @@
  */
 
 package riscv_pkg; 
-  struct instruction_t {
-    logic is_imm;
-  };
+
+// opcode field values
+parameter  OP_LUI       =  5'b01101;
+parameter  OP_AUIPC     =  5'b00101;
+parameter  OP_JAL       =  5'b11011;
+parameter  OP_JALR      =  5'b11001;
+parameter  OP_BRANCH    =  5'b11000;
+parameter  OP_LOAD      =  5'b00000;
+parameter  OP_STORE     =  5'b01000;
+parameter  OP_ALU_IMM   =  5'b00100;
+parameter  OP_ALU       =  5'b01100;
+parameter  OP_MISC_MEM  =  5'b00011;
+parameter  OP_SYSTEM    =  5'b11100;
+
+// branch funct3 field
+parameter  F3_BRANCH_BEQ        =  3'b000;
+parameter  F3_BRANCH_BNE        =  3'b001;
+parameter  F3_BRANCH_BLT        =  3'b100;
+parameter  F3_BRANCH_BGE        =  3'b101;
+parameter  F3_BRANCH_BLTU       =  3'b110;
+parameter  F3_BRANCH_BGEU       =  3'b111;
+// load funct3 field
+parameter  F3_LOAD_LB           =  3'b000;
+parameter  F3_LOAD_LH           =  3'b001;
+parameter  F3_LOAD_LW           =  3'b010;
+parameter  F3_LOAD_LBU          =  3'b100;
+parameter  F3_LOAD_LHU          =  3'b101;
+// store funct3 field
+parameter  F3_STORE_SB          =  3'b000;
+parameter  F3_STORE_SH          =  3'b001;
+parameter  F3_STORE_SW          =  3'b010;
+// alu_imm funct3 field
+parameter  F3_ALU_IMM_ADDI      =  3'b000;
+parameter  F3_ALU_IMM_SLTI      =  3'b010;
+parameter  F3_ALU_IMM_SLTIU     =  3'b011;
+parameter  F3_ALU_IMM_XORI      =  3'b100;
+parameter  F3_ALU_IMM_ORI       =  3'b110;
+parameter  F3_ALU_IMM_ANDI      =  3'b111;
+parameter  F3_ALU_IMM_SLLI      =  3'b001;
+parameter  F3_ALU_IMM_SR        =  3'b101;
+// alu funct3 field
+parameter  F3_ALU_ADD           =  3'b000;
+parameter  F3_ALU_SLL           =  3'b001;
+parameter  F3_ALU_SLT           =  3'b010;
+parameter  F3_ALU_SLTU          =  3'b011;
+parameter  F3_ALU_XOR           =  3'b100;
+parameter  F3_ALU_SR            =  3'b101;
+parameter  F3_ALU_OR            =  3'b110;
+parameter  F3_ALU_AND           =  3'b111;
+// misc_mem funct3 field
+parameter  F3_MISC_MEM_FENCE    =  3'b000;
+parameter  F3_MISC_MEM_FENCE_I  =  3'b001;
+// system funct3 field
+parameter  F3_SYSTEM_CSRRW      =  3'b001;
+parameter  F3_SYSTEM_CSRRS      =  3'b010;
+parameter  F3_SYSTEM_CSRRC      =  3'b011;
+parameter  F3_SYSTEM_CSRRWI     =  3'b101;
+parameter  F3_SYSTEM_CSRRSI     =  3'b110;
+parameter  F3_SYSTEM_CSRRCI     =  3'b111;
+
 endpackage // riscv_pkg
