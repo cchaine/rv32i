4be947c79869af7c75949efb26cf4fe069f9e895
Clément Chaine <cc.chaine@free.fr>
2022-06-26 13:38:14 +0200

    add pipeline intermediate signals in top level

--- src/rv32i.sv
+++ src/rv32i.sv
@@ -27,6 +27,19 @@ module rv32i (
 );
   logic[31:0] instruction;
 
+  logic[2:0] id_alu_op;
+  logic id_alu_alt_op;
+  logic[4:0] id_reg_raddra, id_reg_raddrb;
+  logic[31:0] id_imm;
+  logic id_is_imm;
+
+  logic[4:0] reg_raddra, reg_raddrb;
+  logic[31:0] reg_rdataa, reg_rdatab;
+
+  logic[2:0] ex_alu_op;
+  logic ex_alu_alt_op;
+  logic[31:0] ex_operand1, ex_operand2;
+
   ifstage inst_ifstage (
     .clk_i ( clk_i ),
     .rst_i ( rst_i ),
@@ -34,21 +47,44 @@ module rv32i (
   );
 
   idstage inst_idstage (
-    .clk_i ( clk_i ),
-    .instruction_i ( instruction )
-    .alu_op_o ( id_alu_op_o ),
-    .alu_alt_op_o ( id_alu_alt_op_o ),
-    .reg_raddra_o ( id_reg_addr_a_o ),
-    .reg_raddrb_o ( id_reg_addr_b_o ),
-    .imm_o ( id_imm ),
-    .is_imm_o ( id_is_imm )
+    .clk_i          (  clk_i          ),
+    .instruction_i  (  instruction    )
+    .alu_op_o       (  id_alu_op      ),
+    .alu_alt_op_o   (  id_alu_alt_op  ),
+    .reg_raddra_o   (  id_reg_addra   ),
+    .reg_raddrb_o   (  id_reg_addrb   ),
+    .imm_o          (  id_imm         ),
+    .is_imm_o       (  id_is_imm      )
   );
 
-  exstage inst_exstage (
-    .alu_op_i ( ),
-    .operand1_i ( ),
-    .operand2_i ( ),
+  regfile inst_regfile (
+    .clk_i ( clk_i ),
+    .rst_i ( rst_i ),
+    .write_i ( ),
+    .waddr_i ( ),
+    .wdata_i ( ),
+    .raddra_i ( reg_raddra ),
+    .raddrb_i ( reg_raddrb ),
+    .rdataa_o ( reg_rdataa ),
+    .rdatab_o ( reg_rdatab )
   );
+  always_comb begin
+    reg_raddra = id_reg_raddra;
+    reg_raddrb = id_reg_raddrb;
+  end
+
+  exstage inst_exstage (
+    .alu_op_i      (  ex_alu_op      ),
+    .alu_alt_op_i  (  ex_alu_alt_op  ),
+    .operand1_i    (  ex_operand1    ),
+    .operand2_i    (  ex_operand2    ),
+  );
+  always_comb begin
+    ex_alu_op      =  id_alu_op,
+    ex_alu_alt_op  =  id_alu_alt_op,
+    ex_operand1    =  reg_rdataa;
+    ex_operand2    =  id_is_imm ? id_imm : reg_datab;
+  end
 
   wbstage inst_wbstage (
   
